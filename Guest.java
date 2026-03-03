34e809000a3676bcf83af8acf14e450a5932d903
alhmsiSWE <ffahad7120@gmail.com>
2026-03-03 19:47:35 +0300

    Implement Guest class with Searchable interface

--- Guest.java
+++ Guest.java
@@ -1,5 +1,30 @@
 package Team;
 
-public class Guest {
+public class Guest implements Searchable {
+	private String guestId;
+	private String name;
+	private String phone;
+	private String email;
+	
+	
+	public Guest(String guestId, String name, String phone, String email) {
+		this.guestId = guestId;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+	}
+
+
+	@Override
+	public boolean matchesKeyword(String keyword) {
+		if ( guestId.equalsIgnoreCase(keyword) ||  name.equalsIgnoreCase(keyword) || phone.equalsIgnoreCase(keyword) || email.equalsIgnoreCase(keyword) ) {
+			System.out.println("Guest is here <> ");
+			return true;
+		}
+		else
+			System.out.println("Guset is not here >< ");
+		return false;
+	}
+	
 
 }
