842cf681665436ff069df3653a82c61571447570
alhmsiSWE <ffahad7120@gmail.com>
2026-03-04 19:47:16 +0300

    Add files via upload

---
+++ HotelFahad.java
@@ -0,0 +1,49 @@
+package Team;
+
+public class HotelFahad {
+	
+	private Room[] rooms;
+	private Guest[] guests;
+	private int roomCount;
+	private int guestCount;
+	
+	public boolean addGuest(Guest guest) {
+		
+		return false;
+		
+	}
+	
+	public boolean removeGuest(String guestId) {
+
+		
+	return false;
+
+}
+	
+	public Guest searchGuest(String guestId) {
+		
+		
+		return null;
+	}
+ 	
+	
+	public boolean addRoom(Room room) {
+		
+		return false;
+		
+	}
+	
+	public boolean removeRoom(int roomNumber) {
+
+		
+	return false;
+
+}
+	
+	public Room searchRoom(int roomNumber) {
+		
+		
+		return null;
+	}
+	
+}
