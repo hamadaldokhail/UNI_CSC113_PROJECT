6e4a3effbc616938f3392d1b7fdd7e35625876c4
hamadaldokhail <80049691+hamadaldokhail@users.noreply.github.com>
2026-03-11 00:10:43 +0300

    Refactor Room class implementation

--- Room.java
+++ Room.java
@@ -4,82 +4,69 @@ public class Room implements Searchable {
 	private String type;
 	private double pricePerNight;
 	private boolean available;
-	Reservation reservation;
-	
+
 	public Room(int roomNumber, String type, double pricePerNight) {
-		setRoomNumber(roomNumber); 
+		setRoomNumber(roomNumber);
 		this.type = type;
 		setPricePerNight(pricePerNight);
 		available = true;
 	}
-	
-	
+
 	public int getRoomNumber() {
 		return roomNumber;
 	}
 
-
 	public void setRoomNumber(int roomNumber) {
-			if (roomNumber > 0) 
-				this.roomNumber = roomNumber;
-			else
-				System.out.println("Number can not be negative, try again ");
+		if (roomNumber > 0)
+			this.roomNumber = roomNumber;
+		else
+			System.out.println("Number can not be negative, try again ");
 	}
-
 
 	public String getType() {
 		return type;
 	}
 
-
 	public void setType(String type) {
 		this.type = type;
 	}
-
 
 	public double getPricePerNight() {
 		return pricePerNight;
 	}
 
-
 	public void setPricePerNight(double pricePerNight) {
 		if (pricePerNight > 0)
-		this.pricePerNight = pricePerNight;
-		
+			this.pricePerNight = pricePerNight;
+
 		else
 			System.out.println("price can not be negative, try again ");
 	}
-
 
 	public boolean isAvailable() {
 		return available;
 	}
 
-
 	public void setAvailable(boolean available) {
 		this.available = available;
 	}
 
-
 	public String reserve() {
-	available = false;
+		available = false;
 		return "Room" + roomNumber + "is now reserved <> ";
 	}
+
 	public String release() {
 		available = true;
-		return "Room" + roomNumber + "is now release >< ";
+		return "Room" + roomNumber + "is now released >< ";
 	}
+
 	@Override
 	public boolean matchesKeyword(String keyword) {
-			if (type.equalsIgnoreCase(keyword) ) {
-				return true;
-			}
+		if (type.equalsIgnoreCase(keyword)) {
+			return true;
+		}
 		return false;
 	}
-	
-	
 
 }
-
-
-
