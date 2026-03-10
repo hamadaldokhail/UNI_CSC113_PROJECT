138a42ca0a24791ce491dd8e66f028538163024e
hamadaldokhail <80049691+hamadaldokhail@users.noreply.github.com>
2026-03-11 00:05:59 +0300

    Refactor Hotel class to improve room and guest management

--- Hotel.java
+++ Hotel.java
@@ -1,46 +1,132 @@
 
 public class Hotel {
 
-	
 	private String hotelName;
 	private String address;
-	private Reservation[] reservations;
-	private int reservationCount;
 	private Room[] rooms;
 	private Guest[] guests;
+	private Reservation[] reservations;
 	private int roomCount;
 	private int guestCount;
+	private int reservationCount;
 
-	
 	public Hotel() {
 		this.rooms = new Room[100];
-		this.guests= new Guest[100];
+		this.guests = new Guest[100];
 		this.reservations = new Reservation[100];
-		reservationCount = 0;
+		roomCount = 0;
 		guestCount = 0;
+		reservationCount = 0;
 	}
+
 	public int getRoomCount() {
-		
+
 		return roomCount;
 	}
-	
+
+	// adding room
+	public boolean addRoom(Room room) {
+		if (roomCount < rooms.length) {
+			rooms[roomCount] = room;
+			roomCount++;
+			System.out.println("Room has been added successfully!");
+			return true;
+		}
+
+		System.out.println("No rooms available currently!");
+		return false;
+
+	}
+
+	// removing room
+	public boolean removeRoom(int roomNumber) {
+		for (int i = 0; i < roomCount; i++) {
+			if (rooms[i].getRoomNumber() == roomNumber) {
+				for (int j = i; j < roomCount - 1; j++) {
+					rooms[j] = rooms[j + 1];
+				}
+				rooms[roomCount - 1] = null;
+				roomCount--;
+				System.out.println("Room has been removed successfully!");
+				return true;
+			}
+		}
+		System.out.println("Room not found!");
+		return false;
+
+	}
+
+	// searching for a room
+	public Room searchRoom(int roomNumber) {
+		for (int i = 0; i < roomCount; i++) {
+			if (rooms[i] != null && rooms[i].getRoomNumber() == roomNumber) {
+				return rooms[i];
+			}
+		}
+		return null;
+	}
+
+	// adding guest
+	public boolean addGuest(Guest guest) {
+
+		if (guestCount < guests.length) {
+			guests[guestCount] = guest;
+			guestCount++;
+			System.out.println("Guest has been added successfully!");
+			return true;
+		} else
+			System.out.println("Sorry we do not have enough spaces!");
+
+		return false;
+
+	}
+
+	// removing guests
+	public boolean removeGuest(String guestId) {
+		for (int i = 0; i < guestCount; i++) {
+			if (guests[i].getGuestId().equalsIgnoreCase(guestId)) {
+				for (int j = i; j < guestCount - 1; j++) {
+					guests[j] = guests[j + 1];
+				}
+				guests[guestCount - 1] = null;
+				guestCount--;
+				System.out.println("Guest has been removed successfully!");
+				return true;
+			}
+		}
+		System.out.println("Guest not found!");
+		return false;
+	}
+
+	// searching for a guest
+	public Guest searchGuest(String guestId) {
+		for (int i = 0; i < guestCount; i++) {
+			if (guests[i].matchesKeyword(guestId)) {
+				System.out.println("Guest found!");
+				return guests[i];
+			}
+		}
+		System.out.println("Guest is not found!");
+		return null;
+	}
+
 	// Adding a reservation
 	public boolean addReservation(Reservation reservation) {
 		// check if there is available capacity
 		if (reservationCount == reservations.length) {
-			System.out.println("No reservations available currently");
+			System.out.println("No reservations available currently!");
 			return false;
 		}
 		// check if the reservation is already there
 		for (int i = 0; i < reservationCount; i++) {
 			if (reservations[i].reservationId.equals(reservation.reservationId)) {
-				System.out.println("Its already reserved");
+				System.out.println("Its already reserved!");
 				return false;
 			}
 		}
 		// then add the reservation in the first null value
 		reservations[reservationCount] = reservation;
-		System.out.println("Reservation added successfully!");
+		System.out.println("Reservation has been added successfully!");
 		reservationCount++;
 		return true;
 	}
@@ -55,7 +141,7 @@ public class Hotel {
 		}
 		// if not found
 		if (index == -1) {
-			System.out.println("Reservation not found");
+			System.out.println("Reservation not found!");
 			return false;
 		}
 
@@ -64,7 +150,7 @@ public class Hotel {
 			reservations[i] = reservations[i + 1];
 		}
 		reservations[reservationCount - 1] = null;
-		System.out.println("Reservation cancelled successfully!");
+		System.out.println("Reservation has been cancelled successfully!");
 		reservationCount--;
 
 		return true;
@@ -74,7 +160,6 @@ public class Hotel {
 	public Reservation searchReservation(String reservationId) {
 		for (int i = 0; i < reservationCount; i++) {
 			if (reservations[i].reservationId.equals(reservationId)) {
-				System.out.println("Reservation found!");
 				return reservations[i];
 			}
 		}
@@ -82,10 +167,28 @@ public class Hotel {
 		return null;
 	}
 
+	// count available rooms
+	public int countAvailableRoomRecuresive(int index) {
+
+		if (index >= roomCount) {
+
+			return index;
+		}
+
+		if (rooms[index] == null) {
+			return 1 + countAvailableRoomRecuresive(index + 1);
+		}
+
+		else {
+			return countAvailableRoomRecuresive(index + 1);
+		}
+
+	}
+
 	// showing all reservations if there is any
 	public void displayAllReservations() {
 		if (reservationCount == 0) {
-			System.out.println("there is no reservations yet!");
+			System.out.println("There is no reservations yet!");
 			return;
 		}
 		for (int i = 0; i < reservationCount; i++) {
@@ -93,106 +196,4 @@ public class Hotel {
 		}
 	}
 
-	public boolean addGuest(Guest guest)  {
-		
-		if (guestCount < guests.length) {
-			guests[guestCount] = guest;
-			guestCount++;
-			System.out.println("Guest add successfully ");
-			return true;
-		}
-		else
-			System.out.println("Sorry we do not have enough spaces ");
-			
-		return false;
-		
-	}
-	
-	public boolean removeGuest(String guestId){
-		for (int i = 0; i < guestCount; i++) {
-			if (guests[i].getGuestId().equalsIgnoreCase(guestId)) {
-				for (int j = i; j < guestCount - 1; j++) {
-					guests[j] = guests[j + 1];
-				}
-				guests[guestCount - 1] = null;
-				guestCount--;
-				return true;
-			}
-		}
-
-		return false;
-	}
-	
-	
-	public Guest searchGuest(String guestId) {
-		for (int i = 0; i < guestCount; i++) {
-			if (guests[i].matchesKeyword(guestId)) {
-				System.out.println("The guest is found ");
-				return guests[i];
-			}
-		}
-		System.out.println("Sorry not found ");
-		return null;
-	}
-	public boolean addRoom(Room room) {
-		if (roomCount < rooms.length) {
-			rooms[roomCount] = room;
-			roomCount++;
-			System.out.println("Room add successfully ");
-			return true;
-		}
-
-		System.out.println("Sorry Rooms are full ");
-		return false;
-
-	}
-	public boolean removeRoom(int roomNumber){
-		for (int i = 0; i < roomCount; i++) {
-			if (rooms[i].getRoomNumber() == roomNumber) {
-				for (int j = i; j < roomCount - 1; j++) {
-					rooms[j] = rooms[j + 1];
-				}
-				rooms[roomCount - 1] = null;
-				roomCount--;
-				return true;
-			}
-		}
-
-		return false;
-
-	}
-	public Room searchRoom(int roomNumber){
-		for (int i = 0; i < roomCount; i++) {
-			if (rooms[i]!=null && rooms[i].getRoomNumber()==roomNumber) {
-				return rooms[i];
-			}
-		}
-		return null;
-	}
-	
-	
-	
-	public int countAvailableRoomRecuresive(int index) {
-		
-		if(index>=roomCount ) {
-			
-		return index;
-		}
-		
-		if(rooms[index]==null ) {
-          return 1 + countAvailableRoomRecuresive(index+1);	}
-	
-		else {
-			return countAvailableRoomRecuresive(index+1);}
-		
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
 }
